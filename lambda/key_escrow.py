1410f51233f2a6c7596bd722e89d6a2cc5ba187c
Brian P. Maloney <3286425+brian-maloney@users.noreply.github.com>
2023-02-12 17:04:41 -0500

    Return raw output instead of formatted

--- lambda/key_escrow.py
+++ lambda/key_escrow.py
@@ -2,6 +2,7 @@ import boto3
 import configparser
 import io
 import json
+from base64 import b64encode
 import duo_client
 
 s3 = boto3.resource('s3')
@@ -25,7 +26,27 @@ def lambda_handler(event, context):
         factor='push',
     )
 
-    if response['result'] == 'allow':
-        response['key'] = s3.Object('vond-key-escrow', 'keys/' + request['key']).get()['Body'].read().decode('utf-8')
+    return_struct = {
+        'statusCode': 200,
+            'headers': {
+                'Content-Type': ''
+            },
+            'body': '',
+            'isBase64Encoded': False
+    }
 
-    return response
+
+    if response['result'] == 'allow':
+        obj = s3.Object('vond-key-escrow', 'keys/' + request['key']).get()
+        return_struct['headers']['Content-Type'] = obj['ContentType']
+        if obj['ContentType'].endswith('octet-stream'):
+            return_struct['isBase64Encoded'] = True
+            return_struct['body'] = b64encode(obj['Body'].read())
+        else:
+            return_struct['body'] = obj['Body'].read().decode('utf-8')
+
+    else:
+        return_struct['statusCode'] = 403
+        return_struct['body'] = f"Disallowed due to response of type: {response['result']}\n"
+
+    return return_struct
